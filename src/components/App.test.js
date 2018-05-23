ae5431440f8d5e87b1382e6c4c6e7b15a4752733
HarryFray <harry.fray7@gmail.com>
2018-05-22 19:10:24 -0500

    Configured for redux, enzyme, react, jest

---
+++ src/components/App.test.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import '../setupTests.js'
+import App from './App'
+
+
+describe('App', () => {
+  const app = shallow(<App />)
+
+  it('Should render the App component', () => {
+    expect(app).toMatchSnapshot()
+  })
+})
